fc44e238e92bad756b7b862a9334adcdf85a7d32
Jermaine Williams <wjermaine608@gmail.com>
2025-03-13 00:14:42 +0000

    Simple Counter

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
+import Counter from "./Counter"
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	let [seconds, setSeconds] = useState(0)
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	setInterval(() => {
+		setSeconds(seconds++)
+		
+	}, 20);
+
+	return(
+		<div className="text-center">
+			<Counter seconds={seconds}/>
 		</div>
 	);
 };
